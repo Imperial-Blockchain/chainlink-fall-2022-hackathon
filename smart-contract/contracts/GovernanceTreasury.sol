8bb745ee00eb9dffed9e0de68ab1c52edd778522
Niceural <nicolas.bayle20@imperial.ac.uk>
2022-10-29 12:42:27 +0100

    price feed

--- smart-contract/contracts/GovernanceTreasury.sol
+++ smart-contract/contracts/GovernanceTreasury.sol
@@ -5,32 +5,44 @@ import "./interfaces/IGovernanceTreasury.sol";
 import "./interfaces/IGovernanceRegistry.sol";
 import "./interfaces/ITokenRegistry.sol";
 import "./interfaces/IGovernanceToken.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IERC20Decimals {
+    function decimals() external view returns (uint8);
+}
 
 contract GovernanceTreasury is IGovernanceTreasury {
     IGovernanceRegistry private immutable _registry;
+    mapping(address => address) private _priceFeeds;
 
     constructor(IGovernanceRegistry registry_) {
         _registry = registry_;
     }
 
-    function deposit(
-        address token,
-        address from,
-        uint256 amount
-    ) external payable override {
+    /// @dev If ETH is sent, the function arguments are ignored.
+    function deposit(address token, uint256 amount) external payable override {
         IGovernanceToken govToken = IGovernanceToken(_registry.governanceToken());
-        if (token == address(0)) {
+        if (msg.value > 0) {
             // if ETH
-            require(msg.value > 0, "No Funds");
             govToken.mint(msg.sender, msg.value);
+            emit Deposited(address(0), msg.value, msg.value);
         } else {
             // if not ETH
-            require(msg.value == 0); // to make sure they don't send eth by mistake
-            // check token is authorized
+            // price feed exists, i.e. token is authorized
+            // this contract approved to transferFrom amount
         }
     }
 
     function registry() external view override returns (address) {
         return address(_registry);
+    }
+
+    /// @dev Logic to get the amount of token votes to transfer: $$p_f \frac{p_t d_t}{p_{ETH} d_{ETH}} \frac{d_{ETH}}{d_t} = p_f d_f
+    function _getAmountVoteToken(address tokenAddr) internal returns (uint256) {
+        AggregatorV3Interface feed = AggregatorV3Interface(_priceFeeds[tokenAddr]);
+        uint256 tokenDecimals = IERC20Decimals(tokenAddr).decimals(); // the decimals function is not mandatory according to EIP20
+        uint256 decimals = uint256(feed.decimals());
+        (, int256 price, , , ) = feed.latestRoundData();
     }
 }
--- smart-contract/contracts/interfaces/IGovernanceTreasury.sol
+++ smart-contract/contracts/interfaces/IGovernanceTreasury.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.9;
 
 interface IGovernanceTreasury {
-    function deposit(
-        address token,
-        address from,
-        uint256 amount
-    ) external payable;
+    event Deposited(
+        address indexed token,
+        uint256 indexed tokenAmount,
+        uint256 indexed voteTokenAmount
+    );
+
+    function deposit(address token, uint256 amount) external payable;
 
     // function sendFunds(address token, address to, uint256 amount) external;
     // function deposit(address token, address from, uint256 amount) external;
